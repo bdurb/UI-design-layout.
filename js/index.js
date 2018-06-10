200b9ff0bc39e3bec41f2f095bcc9c3b2c951697
bdurb <brian@rpmgreenville.com>
2018-06-09 22:28:57 -0700

    adjusted styling with nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+
+const showNav = () => {
+  nav.style.position = "fixed";
+  nav.style.top = 0;
+  nav.style.backgroundColor = "#9DA7B1";
+  nav.style.opacity = 0.95;
+};
+const nav = document.querySelector(".navigation");
+window.addEventListener("scroll", showNav);
